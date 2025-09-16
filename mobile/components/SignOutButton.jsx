5007cba424465f4bc7ff8eb874e7334652636b0c
kisekiiv <ivanfernand1@gmail.com>
2025-09-16 16:07:09 +0700

    commit

---
+++ mobile/components/SignOutButton.jsx
@@ -0,0 +1,26 @@
+import { useClerk } from '@clerk/clerk-expo'
+import * as Linking from 'expo-linking'
+import { Text, TouchableOpacity } from 'react-native'
+import { styles } from '../assets/styles/home.styles'
+import { Ionicons } from '@expo/vector-icons';
+import { Alert } from 'react-native'
+import { COLORS } from '../constants/colors.js'
+
+export const SignOutButton = () => {
+  // Use `useClerk()` to access the `signOut()` function
+  const { signOut } = useClerk()
+  const handleSignOut = async () => {
+    
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      { text: "Cancel", style: "cancel" },
+      { text: "Logout", style: "destructive", onPress: signOut },
+    ]);
+  }
+
+
+  return (
+    <TouchableOpacity style={styles.logoutButton} onPress={handleSignOut}>
+      <Ionicons name="log-out-outline" size={24} color={COLORS.text} />
+    </TouchableOpacity>
+  )
+}
---
+++ mobile/constants/api.js
@@ -0,0 +1,2 @@
+// const API_URL = "https://walletmy.onrender.com/api" // for production
+export const API_URL = "http://10.0.2.2:5001/api"; // for local testing with emulator android
